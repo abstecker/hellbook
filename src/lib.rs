18532dc6316b25d75848d79087255882d505203f
folkengine <bower@folkengine.example>
2026-09-19 00:05:00 +0000

    round 3: hello_world, one underscore
    Book-Source: hellbook/src/p3-03-round-3-the-name.md#step-r3-name
    Book-Url: https://abstecker.github.io/hellbook/p3-03-round-3-the-name.html#step-r3-name
    Bower-Step: hellbook/005
    Generated-By: bower v0.1.2

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,7 @@ impl Hello {
         format!("Hello,  {}!", name)
     }
 
-    pub fn  hello__world() -> String {
+    pub fn  hello_world() -> String {
         Hello::hello("wirld!".to_string())
     }
 }
@@ -28,6 +28,6 @@ mod tests {
 
     #[test]
     fn hello_world() {
-        assert_eq!("Hello, world!", Hello::hello__world());
+        assert_eq!("Hello, world!", Hello::hello_world());
     }
 }
